63fcbcc929ea3af25b784ff47d7f46d0fae32879
Thielison Alves <thielisonalves@gmail.com>
2022-06-17 16:06:54 -0300

    Starting to implement Board and Piece

---
+++ src/boardgame/Piece.java
@@ -0,0 +1,19 @@
+package boardgame;
+
+public class Piece {
+
+	protected Position position;
+	private Board board;
+	
+	public Piece(Board board) { 	// para criar uma peça, precisa informar o Tabuleiro
+		this.board = board;
+		position = null; 			// a posição de uma peça recém criada será Null
+	}
+
+	protected Board getBoard() { 	// protected: somente classes dentro do mesmo pacote e subclasses que vão poder acessar o Tabuleiro onde estão as peças
+		return board;
+	}
+
+	// setBoard não é necessário, não será permitido que o Tabuleiro da peça seja alterado
+	
+}
