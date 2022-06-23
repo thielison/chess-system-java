bbe76559ddc9c1cdb35f3b7df0e19dd1bca4a5fd
Thielison Alves <thielisonalves@gmail.com>
2022-06-23 16:10:40 -0300

    Queen (and Pawn bugfix)

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -11,6 +11,7 @@ import chess.pieces.Bishop;
 import chess.pieces.King;
 import chess.pieces.Knight;
 import chess.pieces.Pawn;
+import chess.pieces.Queen;
 import chess.pieces.Rook;
 
 public class ChessMatch {
@@ -84,7 +85,7 @@ public class ChessMatch {
 		// a propriedade check recebe -> se testCheck do oponente for verdade, retorna true, a partida está em check, senão retorna false, que não está em check
 		check = (testCheck(opponent(currentPlayer))) ? true : false;
 		
-		if (testCheckMate(opponent(currentPlayer))) { // se a jogada que eu fiz deixou o oponente e checkMate, o jogo vai ter que acabar
+		if (testCheckMate(opponent(currentPlayer))) { // se a jogada que eu fiz deixou o oponente em checkMate, o jogo vai ter que acabar
 			checkMate = true; // checkMate é true e acaba a partida
 		}
 		else { // caso não esteja em checkMate
@@ -205,6 +206,7 @@ public class ChessMatch {
 		placeNewPiece('a', 1, new Rook(board, Color.WHITE));
         placeNewPiece('b', 1, new Knight(board, Color.WHITE));
         placeNewPiece('c', 1, new Bishop(board, Color.WHITE));
+        placeNewPiece('d', 1, new Queen(board, Color.WHITE));
         placeNewPiece('e', 1, new King(board, Color.WHITE));
         placeNewPiece('f', 1, new Bishop(board, Color.WHITE));
         placeNewPiece('g', 1, new Knight(board, Color.WHITE));
@@ -217,10 +219,11 @@ public class ChessMatch {
         placeNewPiece('f', 2, new Pawn(board, Color.WHITE));
         placeNewPiece('g', 2, new Pawn(board, Color.WHITE));
         placeNewPiece('h', 2, new Pawn(board, Color.WHITE));
-
+        
         placeNewPiece('a', 8, new Rook(board, Color.BLACK));
         placeNewPiece('b', 8, new Knight(board, Color.BLACK));
         placeNewPiece('c', 8, new Bishop(board, Color.BLACK));
+        placeNewPiece('d', 8, new Queen(board, Color.BLACK));
         placeNewPiece('e', 8, new King(board, Color.BLACK));
         placeNewPiece('f', 8, new Bishop(board, Color.BLACK));
         placeNewPiece('g', 8, new Knight(board, Color.BLACK));
